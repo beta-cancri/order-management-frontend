abe1728ff1ead510cdd6a78858d4e50ec6e75ef5
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-10-27 17:15:12 -0500

    register

--- order-management-frontend/src/app/shared/components/login-modal/login-modal.component.ts
+++ order-management-frontend/src/app/shared/components/login-modal/login-modal.component.ts
@@ -4,18 +4,24 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [CommonModule], // Import CommonModule to enable *ngIf
+  imports: [CommonModule],
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.css']
 })
 export class LoginModalComponent {
   isVisible = false;
+  showLogin = true; // Toggle for login or signup view
 
   open() {
     this.isVisible = true;
+    this.showLogin = true; // Default to login view when opened
   }
 
   close() {
     this.isVisible = false;
+  }
+
+  toggleView() {
+    this.showLogin = !this.showLogin;
   }
 }
