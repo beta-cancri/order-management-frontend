ded4c36cde68d788ee98ea09fd372da0f71c48a9
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-10-27 21:59:33 -0500

    fixed navbar home button

--- order-management-frontend/src/app/shared/components/navbar/navbar.component.ts
+++ order-management-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // Import RouterModule
 import { LoginModalComponent } from '../login-modal/login-modal.component';
 import { AuthService } from '../../../core/services/auth.services';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, LoginModalComponent],
+  imports: [CommonModule, RouterModule, LoginModalComponent], // Add RouterModule here
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
